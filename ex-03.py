84ef545bdf3ab4e3378aff0ee3774d1417778cc5
SkylerFG <contato.lipegarcia@gmail.com>
2025-03-17 19:01:06 -0300

    Create ex-03.py

---
+++ ex-03.py
@@ -0,0 +1,5 @@
+#Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor
+
+numero=int(input ("Digite um número: "))
+numero=numero-1
+print ("O número antecessor é:", numero)
