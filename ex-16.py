9f8f5fbc7725963ccb77e6adba14047e2d0e1554
SkylerFG <contato.lipegarcia@gmail.com>
2025-05-19 19:05:35 -0300

    Update ex-16.py

--- ex-16.py
+++ ex-16.py
@@ -4,7 +4,7 @@
 n1=int(input("Digite a primeira nota: "))
 n2=int(input("Digite a segunda nota: "))
 media=(n1+n2)/2
-if media <= 6:
+if media >= 6:
     print("O aluno foi aprovado com a média", media)
 else:
     print("O aluno foi reprovado com a média", media)
