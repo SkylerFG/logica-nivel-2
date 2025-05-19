35fd64bf2d02590fe8b2dd1ee737c12cf1cbab9c
SkylerFG <contato.lipegarcia@gmail.com>
2025-05-19 19:01:13 -0300

    Create ex-16.py

---
+++ ex-16.py
@@ -0,0 +1,10 @@
+#16) Ler as notas da 1a. e 2a. avaliações de um aluno. Calcular a média aritmética simples e escrever uma mensagem que diga se o aluno foi ou não aprovado (considerar que nota igual ou maior que 6 o aluno é aprovado). Escrever também a média calculada.
+
+
+n1=int(input("Digite a primeira nota: "))
+n2=int(input("Digite a segunda nota: "))
+media=(n1+n2)/2
+if media <= 6:
+    print("O aluno foi aprovado com a média", media)
+else:
+    print("O aluno foi reprovado com a média", media)
