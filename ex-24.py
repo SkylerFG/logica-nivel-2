c40a423ff50b825a029b8875b0c58bae98564e46
SkylerFG <contato.lipegarcia@gmail.com>
2025-06-02 19:42:43 -0300

    Update ex-24.py

--- ex-24.py
+++ ex-24.py
@@ -1,5 +1,6 @@
-#24) Faça um algoritmo para ler: número da conta do cliente, saldo, débito e crédito. Após, calcular e escrever o saldo atual (saldo atual = saldo - débito + crédito). Também testar se saldo atual for maior ou igual a zero escrever a mensagem 'Saldo Positivo', senão escrever a mensagem 'Saldo Negativo'.numero_conta= int(input("Digite o número da sua conta: "))
+#24) Faça um algoritmo para ler: número da conta do cliente, saldo, débito e crédito. Após, calcular e escrever o saldo atual (saldo atual = saldo - débito + crédito). Também testar se saldo atual for maior ou igual a zero escrever a mensagem 'Saldo Positivo', senão escrever a mensagem 'Saldo Negativo'.
 
+numero_conta= int(input("Digite o número da sua conta: "))
 saldo= float(input("Digite seu saldo: "))
 debito= float(input("Digite seu debito: "))
 credito= float(input("Digite seu credito: "))
